722386e2eed088d362ba232d24082a0866c31fae
m70880 <m70880@localhost.localdomain>
2020-10-11 15:41:13 +0800

    new

---
+++ cppjieba/test/try.cpp
@@ -0,0 +1,31 @@
+
+const char * const CPP_PATH = "../include/cppjieba/Jieba.hpp";
+#include "../third_part/include/cppjieba/Jieba.hpp"
+
+using namespace std;
+
+const char* const DICT_PATH = "../dict/jieba.dict.utf8";
+const char* const HMM_PATH = "../dict/hmm_model.utf8";
+const char* const USER_DICT_PATH = "../dict/user.dict.utf8";
+const char* const IDF_PATH = "../dict/idf.utf8";
+const char* const STOP_WORD_PATH = "../dict/stop_words.utf8";
+
+int main(int argc, char** argv) {
+  cppjieba::Jieba jieba(DICT_PATH,
+        HMM_PATH,
+        USER_DICT_PATH,
+        IDF_PATH,
+        STOP_WORD_PATH);
+  vector<string> words;
+  vector<cppjieba::Word> jiebawords;
+  string s;
+  string result;
+
+  s = "他来到了网易杭研大厦";
+  cout << s << endl;
+  cout << "[demo] Cut With HMM" << endl;
+  jieba.Cut(s, words, true);
+  cout << limonp::Join(words.begin(), words.end(), "/") << endl;
+
+  return EXIT_SUCCESS;
+}
